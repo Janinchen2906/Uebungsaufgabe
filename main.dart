d82328d548bdeb54716bae2539d9119f8dd35b33
Janine Suhr <janinchen2602003@yahoo.com>
2025-03-03 14:31:35 +0100

    Aufgabe 2 hinzugefügt

--- main.dart
+++ main.dart
@@ -11,7 +11,48 @@ else
 print("Die Person darf den Film nicht sehen");
 
 
+// Test 1
+age = 17;
+ hasParentalConsent = true;
+ movieAgeRating = 16; 
 
-  
+if (age >= movieAgeRating || (hasParentalConsent && age >= movieAgeRating - 2)) {
+    print("Test 1: Die Person darf den Film sehen.");
+  } else {
+    print("Test 1: Die Person darf den Film nicht sehen.");
+  }
+
+  //Test 2
+   age = 13;
+   hasParentalConsent = false;
+   movieAgeRating = 16;
+
+  if (age >= movieAgeRating || (hasParentalConsent && age >= movieAgeRating - 2)) {
+    print("Test 1: Die Person darf den Film sehen.");
+  } else {
+    print("Test 1: Die Person darf den Film nicht sehen.");
+  }
+
+  // Aufgabe 2
+  bool isLoggedIn = true;
+  bool isBanned = false;
+  bool isSubscribed = true;
+  int age2 = 18;
+
+  if (!isLoggedIn){
+    print ("Bitte logge dich ein"); }
+    else if (isBanned){
+      print ("Dein Account wurde gesperrt");
+    }else if (!isSubscribed){
+      print ("Bitte abonniere");
+    }else if (age2<18){
+      print ("Du bist zu jung");
+    }else
+    print ("Willkommen");
+ 
+
+
+
+ 
 
 }
